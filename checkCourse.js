623018ee6087ec18aa2ef7cc5d4c3347ccd052df
Chris <dellch@users.noreply.github.com>
2017-01-06 10:08:46 -0500

    Create checkCourse.js

---
+++ checkCourse.js
@@ -0,0 +1,32 @@
+/**
+ * NOTE:  make sure you replace term parameter with the correct term from one.uf course search, currently Spring 2017
+ * Usage:  I use this in a console in chrome/firefox
+ *
+ *
+ * Recursively searches UF's open courses every 30 seconds and alerts if there is an opening
+ * @param {String} course - course id e.g. "enc1101" or "cot3100"
+ * @param {Number} seconds - number of seconds between each search, default = 30, numbers less than 10 default to 30
+ */
+function checkCourse(course, seconds){
+  
+	var req = new XMLHttpRequest()
+    , url = "https://one.uf.edu/api/myschedule/course-search/?category=RES&course-code=" + course + "&prog-level=UGRD&term=20171";
+    
+  if (seconds === undefined || typeof(seconds) != "number" || seconds < 10) seconds = 30;
+	req.overrideMimeType("application/json");
+	req.open("GET", url);
+	req.onload = function(){
+		var data = JSON.parse(req.responseText);
+		if (data.length > 0){
+			var json = JSON.parse(req.responseText)[0];
+			if (json.hasOwnProperty("TOTALROWS")){
+				if (json["TOTALROWS"] > 0){
+					alert(course + " has openings");
+				} else { 
+					setTimeout(function(){checkCourse(course)}, seconds * 1000);
+				}
+			}
+		}
+	}
+	req.send();
+}
